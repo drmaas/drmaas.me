7d2a3bcd4c4ead642a5dded82c8ff69062d0905e
Dan Maas <dmaas@salesforce.com>
2023-06-25 13:00:13 -0500

    adding gatsby
    Author:    Dan Maas <drmaas@gmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,15 +9,15 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Personal site for Dan Maas`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Daniel Robert Maas`,
+      summary: `who writes words and thinks thoughts`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
+    description: `A blog about many things in life`,
+    siteUrl: `https://drmaas.me`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `drmaas`,
     },
   },
   plugins: [
@@ -102,7 +102,7 @@ module.exports = {
               }
             }`,
             output: "/rss.xml",
-            title: "Gatsby Starter Blog RSS Feed",
+            title: "drmaas.me RSS Feed",
           },
         ],
       },
@@ -110,8 +110,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `Gatsby`,
+        name: `Personal site for Dan Maas`,
+        short_name: `Dan Maas`,
         start_url: `/`,
         background_color: `#ffffff`,
         // This will impact how browsers show your PWA/website
