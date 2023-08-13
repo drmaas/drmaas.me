f1e01efd1d59f0db28b0b37e520c9463ee6a59a0
Dan Maas <dmaas@salesforce.com>
2023-08-12 22:58:03 -0500

    feat: new post about services, adding TOC, link fixes

--- content/drafts/how-to-build-microservices/index.md
+++
@@ -1,8 +0,0 @@
----
-title: How to build microservices
-slug: /slug
-date: "2023-07-02T20:15:00.000Z"
-description: "Sometimes you just shouldn't."
----
-
-Template
--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,8 +1,29 @@
 import * as React from "react"
 import { Link, graphql } from "gatsby"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+
+const blogPrefix = '/blog'
+
+const Toc = styled.ul`
+  position: fixed;
+  left: calc(50% + 400px);
+  top: 110px;
+  max-height: 70vh;
+  width: 310px;
+  display: flex;
+  li {
+    line-height: 10px;
+    margin-top: 10px;
+  }
+`
+
+const InnerScroll = styled.div`
+  overflow: hidden;
+  overflow-y: scroll;
+`
 
 const BlogPostTemplate = ({
   data: { previous, next, site, mdx: post },
@@ -20,11 +41,38 @@ const BlogPostTemplate = ({
         itemType="http://schema.org/Article"
       >
         <header>
-          <Link className="header-link-home" to={post.frontmatter.slug}><h1 itemProp="headline">{post.frontmatter.title}</h1></Link>
+          <Link className="header-link-home" to={`${blogPrefix}${post.frontmatter.slug}`}><h1 itemProp="headline">{post.frontmatter.title}</h1></Link>
           <p>{post.frontmatter.date}
             <span> &#8226; {post.fields.timeToRead.text} </span>
           </p>
         </header>
+        {typeof post.tableOfContents.items === 'undefined' ? null : (
+          <Toc>
+            <InnerScroll>
+              {/* Note: this only goes 2 levels deep. Think about making it more flexible and resuable. */}
+              <h2>Table of contents</h2>
+              <ul>
+                {post.tableOfContents.items.map(i => (
+                  <li key={i.url}>
+                    <a href={i.url} key={i.url}>
+                      {i.title}
+                    </a>
+                    <ul>
+                      {i.items?.map(i => (
+                        <li key={i.url}>
+                          <a href={i.url} key={i.url}>
+                            {i.title}
+                          </a>
+                        </li>
+                      ))}
+                    </ul>
+                  </li>
+                ))}
+              </ul>
+              <Link to={`${blogPrefix}${post.frontmatter.slug}`}>Back to Top</Link>
+            </InnerScroll>
+          </Toc>
+        )}
         <section itemProp="articleBody">{children}</section>
         <hr />
         <footer>
@@ -43,14 +91,14 @@ const BlogPostTemplate = ({
         >
           <li>
             {previous && (
-              <Link to={`/blog${previous.frontmatter.slug}`} rel="prev">
+              <Link to={`${blogPrefix}${previous.frontmatter.slug}`} rel="prev">
                 ← {previous.frontmatter.title}
               </Link>
             )}
           </li>
           <li>
             {next && (
-              <Link to={`/blog${next.frontmatter.slug}`} rel="next">
+              <Link to={`${blogPrefix}${next.frontmatter.slug}`} rel="next">
                 {next.frontmatter.title} →
               </Link>
             )}
@@ -89,6 +137,7 @@ export const pageQuery = graphql`
     mdx(id: { eq: $id }) {
       id
       excerpt(pruneLength: 160)
+      tableOfContents
       body
       fields {
         timeToRead {
